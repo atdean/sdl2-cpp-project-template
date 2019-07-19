cd082e4b75bb7389812e06db238a2ba3fad09a78
Austin Dean <austin@atdean.com>
2019-07-19 08:55:59 -0400

    Updated main.cpp to show a blank window and added a note of that into the README.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,34 @@
-#include <iostream>
 #include <SDL2/SDL.h>
 
 int main(int argc, const char* argv[])
 {
+    bool quit = false;
+    SDL_Event event;
+
     SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("SDL2 Starter Project",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-    SDL_Window *window = SDL_CreateWindow(
-        "SDL2Test",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
-        0
-    );
+    while (!quit)
+    {
+        SDL_Delay(10);
+        SDL_PollEvent(&event);
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
+        }
 
-    SDL_Delay(3000);
+        SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
+        SDL_RenderClear(renderer);
 
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
-    return 0;
 }
